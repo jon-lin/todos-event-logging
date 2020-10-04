a237e97e5cb7e047064963dff9215fd3540a503e
Jonathan Lin <jon.cp.lin@gmail.com>
2020-10-03 22:27:30 -0400

    Add CSS reset and Roboto font, basic page styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
-import TodosPanel from './TodosPanel';
+import styled from '@emotion/styled';
+import TodosPanel from './TodosPanel/TodosPanel';
+import './index.css';
 
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const TodosPage = styled.div({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: 24,
+  height: '100vh',
+})
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/',
@@ -11,9 +21,9 @@ const client = new ApolloClient({
 const App = () => {
   return (
     <ApolloProvider client={client}>
-      <div>
+      <TodosPage>
         <TodosPanel />
-      </div>
+      </TodosPage>
     </ApolloProvider>
   );
 }
